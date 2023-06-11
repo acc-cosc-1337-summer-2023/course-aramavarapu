739bc157e257130fb38179be29313b094a048966
aramavarapu <135273286+aramavarapu@users.noreply.github.com>
2023-06-11 20:05:16 +0000

    did my homework for 1

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -1,12 +1,22 @@
 ﻿//write include statements
-
+#include <iostream>
 
 //write namespace using statement for cout
-
+int multiply_numbers(int number) 
+{ 
+	return number*2; 
+}
 
 int main()
 {
-	
+	int num;
+	std::cout<<"Enter a number: ";
+	std::cin >> num;
+	int result = multiply_numbers(num);
+	std::cout<<"Result with num: "<<result;
+	int num1=4;
+	result = multiply_numbers(num1);
+	std::cout<<"Result with num1: "<<result;
 
 	return 0;
 }
