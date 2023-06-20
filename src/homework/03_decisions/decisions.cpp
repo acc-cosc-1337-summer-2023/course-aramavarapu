8362284141630e4f6976da2e5fda6e9b6e6d3bc4
aramavarapu <135273286+aramavarapu@users.noreply.github.com>
2023-06-20 22:34:52 +0000

    changes too hw 2

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -6,39 +6,48 @@
 std::string get_letter_grade_using_switch(int grade) 
 {
 std::string letter_grade;
-switch(grade/10){
-	case 10:
-	case 9:
-	letter_grade="A";
-	break;
-	case 8:
-	letter_grade="B";
-	break;
-	case 7:
-	letter_grade="C";
-	break;
-	case 6:
-	letter_grade="D";
-	break;
-	default:
-	letter_grade="F";
-	break;
-}
-return letter_grade;
+ if (grade < 0) {
+        letter_grade = "Invalid";
+    } else {
+        switch (grade / 10) {
+            case 10:
+            case 9:
+                letter_grade = "A";
+                break;
+            case 8:
+                letter_grade = "B";
+                break;
+            case 7:
+                letter_grade = "C";
+                break;
+            case 6:
+                letter_grade = "D";
+                break;
+            default:
+                letter_grade = "F";
+                break;
+        }
+    }
+    
+    return letter_grade;
 }
 std::string get_letter_grade_using_if(int grade)
 {
-	std::string letter_grade;
-	if(grade>=90){
-		letter_grade="A";
-	} else if (grade>=80){
-		letter_grade="B";
-	} else if (grade>=70){
-		letter_grade="C";
-	} else if (grade>=60){
-		letter_grade="D";
-	} else{
-		letter_grade="F";
-	}
-	return letter_grade;
+ std::string letter_grade;
+    
+    if (grade < 0) {
+        letter_grade = "Invalid";
+    } else if (grade >= 90) {
+        letter_grade = "A";
+    } else if (grade >= 80) {
+        letter_grade = "B";
+    } else if (grade >= 70) {
+        letter_grade = "C";
+    } else if (grade >= 60) {
+        letter_grade = "D";
+    } else {
+        letter_grade = "F";
+    }
+    
+    return letter_grade;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -2,21 +2,21 @@
 #include <iostream>
 #include <string>
 //add function(s) code here
-double get_gc_content(const std::string &DNAString) {
-int totalLetters=0;
-int GCLetter=0;
-for (char symbol : DNAString){
-    if (symbol == 'C'){
-        GCLetter++;
+double get_gc_content(const std::string& DNAString) {
+    int totalLetters = 0;
+    int GCLetter = 0;
+    for (char symbol : DNAString) {
+        if (symbol == 'C') {
+            GCLetter++;
+        }
+        if (symbol == 'G') {
+            GCLetter++;
+        }
+        totalLetters++;
     }
-    if (symbol == 'G'){
-        GCLetter++;
+    if (totalLetters == 0) {
+        return 0.0;
     }
-    totalLetters++;
-}
-if (totalLetters=0){
-    return 0;
-}
-int DNA = (GCLetter * 100) / totalLetters;
-return DNA;
+    double DNA = static_cast<double>(GCLetter) / totalLetters;
+    return DNA;
 }
